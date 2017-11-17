b439c1ea99bb70c4503b28586f25d754c21fd855
Nodes <33381653+TheNodeCoin@users.noreply.github.com>
2017-11-17 13:53:52 -0700

    added seed node

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,6 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "104.131.144.233:8080",
+  "132.148.143.119:8080",
   //"your_seed_ip2.com:8080",
 };
 
